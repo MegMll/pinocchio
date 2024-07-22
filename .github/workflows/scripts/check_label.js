0582f49deb01bc3b883f4bae81cff6ab8aa31755
Mégane Millan <37700908+MegMll@users.noreply.github.com>
2024-07-22 14:45:52 +0200

    Format review message as markdown

--- .github/workflows/scripts/check_label.js
+++ .github/workflows/scripts/check_label.js
@@ -68,20 +68,20 @@ module.exports = async ({github, context, core}) => {
         else
         {       
             const reviewMessage = `👋 Hi,
-            This is a reminder message to please assign a proper label to this Pull Request.
-            The possible labels are:
-            
-                - build_collision (build pinocchio with coal support)
-                - build_casadi (build pinoochio with casadi support)
-                - build_autodiff (build pinocchio with cppad support)
-                - build_codegen (build pinocchio with cppadcg support)
-                - build_extra (build pinocchio with extra algorithms)
-                - build_mpfr (build pinocchio with Boost.Multiprecision support)
-                - build_sdf (build pinocchio with sdf parser)
-                - build_accelerate
-                - build_all (build pinocchio with all the options stated above)
-            
-            Thanks.`;
+This is a reminder message to please assign a proper label to this Pull Request.
+The possible labels are:
+
+- build_collision (build pinocchio with coal support)
+- build_casadi (build pinoochio with casadi support)
+-  build_autodiff (build pinocchio with cppad support)
+- build_codegen (build pinocchio with cppadcg support)
+- build_extra (build pinocchio with extra algorithms)
+- build_mpfr (build pinocchio with Boost.Multiprecision support)
+- build_sdf (build pinocchio with sdf parser)
+- build_accelerate
+- build_all (build pinocchio with all the options stated above)
+
+Thanks.`;
             await github.rest.pulls.createReview({
                 owner:context.repo.owner,
                 repo: context.repo.repo,
