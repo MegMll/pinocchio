7895bb9c7f2c15f2adc27689f0d432f3611421ad
Megane Millan <megane.millan@inria.fr>
2024-09-29 14:31:57 +0200

    [Joints] Added support for nj and idx_j

--- include/pinocchio/multibody/joint/joint-helical-unaligned.hpp
+++ include/pinocchio/multibody/joint/joint-helical-unaligned.hpp
@@ -526,7 +526,8 @@ namespace pinocchio
     enum
     {
       NQ = 1,
-      NV = 1
+      NV = 1, 
+      NJ = 1
     };
     typedef _Scalar Scalar;
     enum
@@ -640,6 +641,7 @@ namespace pinocchio
     using Base::id;
     using Base::idx_q;
     using Base::idx_v;
+    using Base::idx_j;
     using Base::setIndexes;
 
     JointModelHelicalUnalignedTpl()
@@ -760,7 +762,7 @@ namespace pinocchio
     {
       typedef JointModelHelicalUnalignedTpl<NewScalar, Options> ReturnType;
       ReturnType res(axis.template cast<NewScalar>(), ScalarCast<NewScalar, Scalar>::cast(m_pitch));
-      res.setIndexes(id(), idx_q(), idx_v());
+      res.setIndexes(id(), idx_q(), idx_v(), idx_j());
       return res;
     }
 
