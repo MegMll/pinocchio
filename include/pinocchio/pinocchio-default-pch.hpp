0e2b4af7349f1a35575542019c7f1499809df055
Megane Millan <megane.millan@inria.fr>
2025-03-12 17:02:07 +0100

    [cmake] test pch

---
+++ include/pinocchio/pinocchio-default-pch.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "pinocchio/container/boost-container-limits.hpp"
+
+#include <Eigen/Core>
+#include <Eigen/Sparse>
+#include <Eigen/Geometry>
+#include <Eigen/SparseCholesky>
+#include <unsupported/Eigen/CXX11/Tensor>
+
+#include <map>
+#include <iterator>
+#include <sstream>
+
+#include <boost/variant.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/math/constants/constants.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/fusion/algorithm.hpp>
