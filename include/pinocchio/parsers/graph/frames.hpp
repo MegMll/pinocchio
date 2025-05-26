1f8a2ba4d1f2b18e8ebd41b9515d429499b1e94c
Megane Millan <megane.millan@inria.fr>
2025-05-26 17:52:04 +0200

    apply pre-commit

--- include/pinocchio/parsers/graph/frames.hpp
+++ include/pinocchio/parsers/graph/frames.hpp
@@ -11,41 +11,38 @@
 
 namespace pinocchio
 {
-    struct BodyFrameGraph
+  struct BodyFrameGraph
+  {
+    /// @brief Spatial inertia of the body, expressed at its center of mass (CoM).
+    ///
+    /// Note: If the joint is reversed in the model graph, the body frame pose
+    /// is kept the same in the model, so this inertia remains valid.
+    Inertia inertia = pinocchio::Inertia::Identity();
+
+    pinocchio::FrameType f_type = BODY;
+
+    BodyFrameGraph() = default;
+    BodyFrameGraph(const pinocchio::Inertia & in)
+    : inertia(in)
     {
-        /// @brief Spatial inertia of the body, expressed at its center of mass (CoM).
-        ///
-        /// Note: If the joint is reversed in the model graph, the body frame pose
-        /// is kept the same in the model, so this inertia remains valid.
-        Inertia inertia = pinocchio::Inertia::Identity();
+    }
+  };
 
+  struct SensorFrameGraph
+  {
+    pinocchio::FrameType f_type = SENSOR;
 
-        pinocchio::FrameType f_type = BODY;
+    SensorFrameGraph() = default;
+  };
 
-        BodyFrameGraph() = default;    
-        BodyFrameGraph(const pinocchio::Inertia & in):
-        inertia(in)
-        {}
-    };
+  struct OpFrameGraph
+  {
+    pinocchio::FrameType f_type = OP_FRAME;
 
-    struct SensorFrameGraph
-    {
-        pinocchio::FrameType f_type = SENSOR;
-        
-        SensorFrameGraph() = default;
-    };
+    OpFrameGraph() = default;
+  };
 
-    struct OpFrameGraph
-    {
-        pinocchio::FrameType f_type = OP_FRAME;
-        
-        OpFrameGraph() = default;
-    };
+  using FrameGraphVariant = boost::variant<BodyFrameGraph, SensorFrameGraph, OpFrameGraph>;
 
-    using FrameGraphVariant = boost::variant<
-        BodyFrameGraph,
-        SensorFrameGraph,
-        OpFrameGraph>;
-        
 } // namespace pinocchio
 #endif
