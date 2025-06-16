59ce20def428a31d6465811db5cd4b40b0e363f3
Megane Millan <millan.megane@gmail.com>
2025-06-16 17:42:17 +0200

    Example - add python example for creating and using a model graph

---
+++ examples/model-graph.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+import pinocchio as pin
+
+# Create an empty model graph
+g = pin.ModelGraph()
+
+###### Add bodies, sensors... 
+g.addBody("body1", pin.Inertia.Identity())
+g.addBody("body2", pin.Inertia.Identity())
+
+###### Add joints
+g.addJoint(
+    "b1_b2",
+    pin.JointPrismaticGraph(np.array([1, 0, 0])),
+    "body1",
+    pin.SE3.Random(),
+    "body2",
+    pin.SE3.Random(),
+)
+
+# Create model based on graph 
+m = pin.buildModel(g, "body1", pin.SE3.Identity())
+
+print(m)
